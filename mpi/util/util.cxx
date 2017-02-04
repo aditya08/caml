3457b91bacac8fcdf39071104c79f91e4499a83f
Aditya Devarakonda <aditya08@berkeley.edu>
2017-02-03 20:19:13 -0800

    modifying util to perform parallel mpi file i/o

--- mpi/util/util.cxx
+++ mpi/util/util.cxx
@@ -6,33 +6,73 @@
 #include <stdlib.h>
 #include <assert.h>
 
+#include <mpi.h>
+
 #include "util.h"
 
 void libsvmread(const char* fname, int m, int n, double *A, int leny, double *y){
 	int i = 0, idx = 0;
-	std::ifstream file(fname);
-	std::string line;
-	std::size_t pos;
 
-	assert(0!=file.is_open());
-	//std::cout << "about to read from file" << std::endl;
-	
-	while(file){
-		file >> line;
-		pos = line.find(":");
-		if(pos != std::string::npos){
-			//std::cout << "column = " << atoi(line.substr(0,pos).c_str()) << std::endl;
-			idx = n*(i-1) + (atoi(line.substr(0,pos).c_str())-1);
-			//std::cout << "idx = " << idx << std::endl;
-			A[idx] = atof(line.substr(pos+1).c_str());
-		}
-		else{
-			y[i] = atof(line.c_str());
-			++i;
-		}
+	int rank;
+	int npes;
+
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &npes);
+
+	MPI_Offset end;
+	MPI_Offset fsize;
+	MPI_Offset start;
+
+	int localrdsize;
+	int overlap = 100;
+	char *rdchars;
+
+
+	MPI_File in;
+	int ierr = MPI_File_open(MPI_COMM_WORLD, fname, MPI_MODE_RDONLY, MPI_INFO_NULL, &in);
+	assert(!ierr);
+
+	MPI_File_get_size(in, &fsize);
+
+	localrdsize = fsize/npes;
+	start = rank*localrdsize;
+	end = localrdsize + start - 1;
+
+	if(rank == npes - 1) end = fsize - 1;
+
+	if(rank != npes - 1) end += overlap;
+
+	localrdsize = end - start + 1;
+
+	rdchars = Malloc(char,localrdsize);
+
+	MPI_File_read_at_all(in, start, rdchars, localrdsize, MPI_CHAR, MPI_STATUS_IGNORE);
+	rdchars[localrdsize] = '\0';
+
+	if(rank != 0){
+		while(rdchars[start] != '\n')
+			start++;
+		start++;
 	}
 
-	file.close();
+	if(rank != npes - 1){
+		end-= overlap;
+		while(rdchars[end] != '\n')
+			end++;
+	}
+
+	localrdsize = end - start + 1;
+
+	/* Parse file chunk into local CSR/CSC matrices based on primal or dual method.
+	 * Matrix is already in 1D-column layout. Need to perform All_to_allv for 1D-row.
+	 * 
+	 * Also, ensure that nnz per rank is roughly load-balanced.
+	 * Easiest to just construct CSR/CSC matrices and compare length of values vector.
+	 * 
+	 * Compare performance with/without LB and plot running time.
+	 * */
+	
+	MPI_File_close(&in);
 }
 
 void staticLB_1d(int m, int n, int npes, int flag, int *cnts, int *displs, int *cnts2, int *displs2)
